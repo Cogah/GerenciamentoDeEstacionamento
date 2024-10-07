337453d1ebcec04b65f9c121a7e5c6b73abe2906
1drey2drey3drey <165610379+1drey2drey3drey@users.noreply.github.com>
2024-10-07 09:25:35 -0300

    Update App.java

--- Gerenciador de Estacionamento/GerenciaEstacionamento/app/src/main/java/gradlee/App.java
+++ Gerenciador de Estacionamento/GerenciaEstacionamento/app/src/main/java/gradlee/App.java
@@ -1,25 +1,17 @@
 package gradlee;
 
 public class App {
-
-    
-    
     public static void main(String[] args) throws Exception{
-        Gerenciador gerencia = new Gerenciador();
-        GeolocationServiceNominatim geolocationService = new GeolocationServiceNominatim();
-        
-        String endereco = "Avenida Alcindo Cacela 1523, Umarizal, Brazil";// Endereço do estacionamento
-        System.out.println("Coordenadas do estacionamento: ");
-        geolocationService.obterCoordenadas(endereco);
-        
 
-        
+        Gerenciador gerencia = new Gerenciador();
+    
+        gerencia.acharEndereco();    
         gerencia.lerArquivoCSV();
         gerencia.listagem();
 
         do {
             gerencia.menu();
-        }while(gerencia.op != 6);
+        }while(gerencia.op != 5);
 
 
     }
